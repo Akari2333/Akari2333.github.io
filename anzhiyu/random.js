50b490cb9c04cddefd48d2acdff4f4691c74b239
Akari2333 <2910747383@qq.com>
2023-11-04 14:22:16 +0800

    Site updated: 2023-11-04 14:22:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/11/04/hello world/","2023/11/03/blog参考网站/","2023/11/04/发癫日常（1）（Blog测试√）/","2023/11/03/空_无/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/11/03/blog参考网站/","2023/11/04/hello world/","2023/11/04/发癫日常（1）（Blog测试√）/","2023/11/03/空_无/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
